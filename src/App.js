3dc65ca916bb149affd99bc270697b72cb7e352c
TruongQuangMin <minhvvn01@gmail.com>
2025-08-20 22:48:41 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MapComponent from "./components/MapComponent.jsx";
-import MyMap from "./components/MyMap.jsx";
+// import MyMap from "./components/MyMap.jsx";
 import Login from "./pages/login.jsx";
 
 function App() {
